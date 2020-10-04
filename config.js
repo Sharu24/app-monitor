c7e4b2eac4b268c6856814ed2b9377543df0c7f2
ip11 <iprash011@gmail.com>
2020-10-05 03:29:34 +0530

    SMS API

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 const configs = {
     'hashingSecret': 'Code.INT20Hackathon',
-    'maxChecks': 5
+    'maxChecks': 5,
+    'twilio': {
+        'fromPhone': '+12096385801',
+        'accountSid': 'ACc4f195098443386812cfa94a5a9f7e4d',
+        'authToken': '957eeb939536ad3550cb915639c8a5c3'
+    }
 };
 
 module.exports = configs;
